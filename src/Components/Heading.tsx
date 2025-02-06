dcb478323549d557d6fce1f1d32f6ce1b0ce1b9a
BomberQLF <tmurphyparis@gmail.com>
2025-02-06 08:21:10 +0100

    Responsive Accueil

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -7,12 +7,16 @@ interface HeadingProps {
 
 const Heading = ({ title, subtitle }: HeadingProps): JSX.Element => {
     return (
-        <div className="flex items-center flex-col justify-center mt-24">
-            <h1 className="text-white text-[12rem] uppercase">{title}</h1>
-            <h2 className="text-white text-4xl uppercase">{subtitle}</h2>
-            < Play />
+        <div className="flex flex-col items-center justify-center mt-12 px-4 text-center">
+            <h1 className="text-white uppercase text-4xl sm:text-6xl md:text-8xl lg:text-9xl xl:text-[10rem] leading-none">
+                {title}
+            </h1>
+            <h2 className="text-white uppercase text-lg sm:text-xl md:text-2xl lg:text-3xl">
+                {subtitle}
+            </h2>
+            <Play />
         </div>
-    )
-}
+    );
+};
 
 export default Heading;
--- src/Components/Play.tsx
+++ src/Components/Play.tsx
@@ -3,15 +3,19 @@ import Stickman from '../assets/stickman.svg';
 
 const Play = (): JSX.Element => {
     return (
-        <div>
+        <div className="flex flex-col items-center mt-16">
             <Link to="/game">
-                <div className="flex justify-center mt-32 text-4xl border border-white rounded-xl py-[.5rem] px-[5rem]">
-                    <h3 className="text-white">Play Now!</h3>
+                <div className="flex justify-center text-xl sm:text-2xl md:text-3xl lg:text-4xl border border-white rounded-xl py-3 px-8 sm:px-12 md:px-16 lg:px-20 transition hover:bg-white hover:text-black">
+                    <h3 className="text-white font-bold">Play Now!</h3>
                 </div>
             </Link>
-            <img src={Stickman} alt="Stickman" className="relative right-[-6rem]" />
+            <img
+                src={Stickman}
+                alt="Stickman"
+                className="w-8 sm:w-12 md:w-16 lg:w-24 m-0 block"
+            />
         </div>
-    )
-}
+    );
+};
 
 export default Play;
